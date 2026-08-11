a523dcefbe4ea3d245d62adc9a92876efae56aa0
Felix Cos <info@felixcos.de>
2026-08-11 08:47:28 +0200

    fix(android): cover PBS in SSE service mapping

--- HomelabAndroid/app/src/main/java/com/homelab/app/util/SseClient.kt
+++ HomelabAndroid/app/src/main/java/com/homelab/app/util/SseClient.kt
@@ -53,6 +53,7 @@ class SseClient @Inject constructor(
                 ServiceType.WAKAPI -> "Wakapi"
                 ServiceType.CRAFTY_CONTROLLER -> "Crafty Controller"
                 ServiceType.PROXMOX -> "Proxmox"
+                ServiceType.PROXMOX_BACKUP_SERVER -> "Proxmox Backup Server"
                 ServiceType.TRUENAS -> "TrueNAS"
                 ServiceType.PTERODACTYL -> "Pterodactyl"
                 ServiceType.CALAGOPUS -> "Calagopus"
